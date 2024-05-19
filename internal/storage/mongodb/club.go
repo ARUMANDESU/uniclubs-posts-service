96be7e98828969b07dd953d9ec9b3db1f09892cb
ARUMANDESU <zh.arumandes@gmail.com>
2024-05-19 09:18:18 +0500

    update club in db

--- internal/storage/mongodb/club.go
+++ internal/storage/mongodb/club.go
@@ -11,13 +11,13 @@ import (
 func (s Storage) UpdateClub(ctx context.Context, club *domain.Club) error {
 	const op = "storage.mongodb.updateClub"
 
-	_, err := s.eventsCollection.UpdateMany(ctx, bson.M{"collaborator_clubs.id": club.ID}, bson.M{
+	_, err := s.eventsCollection.UpdateMany(ctx, bson.M{"collaborator_clubs._id": club.ID}, bson.M{
 		"$set": bson.M{
 			"collaborator_clubs.$[elem].name":     club.Name,
 			"collaborator_clubs.$[elem].logo_url": club.LogoURL,
 		},
 	}, options.Update().SetArrayFilters(options.ArrayFilters{
-		Filters: []interface{}{bson.M{"elem.id": club.ID}},
+		Filters: []interface{}{bson.M{"elem._id": club.ID}},
 	}))
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
